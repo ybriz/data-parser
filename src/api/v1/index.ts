2f0797ee411e4e33d2f91700b79caf7b16bf3f6a
ybriz <ylbriz82@gmail.com>
2020-02-04 23:59:40 -0500

    fixing url

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -2,5 +2,5 @@ import routes from './routes';
 
 export default (app) => {
   // bootstrap api routes
-  app.use('/api/v1', routes);
+  app.use('/api/v1/parse', routes);
 };
--- src/api/v2/index.ts
+++ src/api/v2/index.ts
@@ -2,5 +2,5 @@ import routes from './routes';
 
 export default (app) => {
   // bootstrap api routes
-  app.use('/api/v2', routes);
+  app.use('/api/v2/parse', routes);
 };
